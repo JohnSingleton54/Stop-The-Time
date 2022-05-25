65b408c80bd554deefe9d1a11af8fa871a73b837
Singleton <j51n974@msu.montana.edu>
2022-05-24 19:09:34 -0600

    I set up this React project in the manner of the Frontend Masters course Complete Intro to React, v7.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { render } from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Stop the time!</h1>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
